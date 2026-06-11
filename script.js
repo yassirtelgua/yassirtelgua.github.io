e72be48843502bd015e2d51b2d3f814f1003b295
yassirtelgua <yassirtel1@gmail.com>
2026-06-11 10:20:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const machines = document.querySelectorAll(".machine");
 
-function animateMachines() {
+window.addEventListener("scroll", () => {
   machines.forEach(machine => {
     const parent = machine.parentElement;
     const rect = parent.getBoundingClientRect();
 
     if (rect.top < window.innerHeight && rect.bottom > 0) {
       machine.style.transform = "translateX(120vw)";
+    } else {
+      machine.style.transform = "translateX(0px)";
     }
   });
-}
-
-window.addEventListener("scroll", animateMachines);
+});
