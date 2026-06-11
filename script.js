62126239d2fe60a33b7aa7d5f9d136c9b4c19dcb
yassirtelgua <yassirtel1@gmail.com>
2026-06-11 10:36:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const machines = document.querySelectorAll(".machine");
+const sections = document.querySelectorAll(".section");
 
 window.addEventListener("scroll", () => {
+
+  // MACHINE ANIMATION
   machines.forEach(machine => {
     const parent = machine.parentElement;
     const rect = parent.getBoundingClientRect();
@@ -11,4 +14,15 @@ window.addEventListener("scroll", () => {
       machine.style.transform = "translateX(0px)";
     }
   });
+
+  // SECTION ANIMATION
+  sections.forEach(section => {
+    const rect = section.getBoundingClientRect();
+
+    if (rect.top < window.innerHeight - 100) {
+      section.classList.add("show");
+    }
+  });
+
 });
+``
